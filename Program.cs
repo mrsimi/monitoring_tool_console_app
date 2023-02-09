69190db30e5f40301b20f964c24bf8b801266bf9
FatherStreet_bySimi <adegokesimi@gmail.com>
2023-02-09 22:22:40 +0100

    create an app

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string websiteAddress = "http://google.com";
+        if (args.Length > 0) websiteAddress = args.Single();
+        new Timer((e) =>
+        {
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(websiteAddress).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"{DateTime.Now}: {websiteAddress} is up and running. Recieved {response.Content.Headers.ContentLength} bytes");
+                }
+                else
+                {
+                    Console.WriteLine($"{DateTime.Now}: {websiteAddress} is down.");
+                }
+            }
+        }, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+        Console.ReadLine();
+    }
+}
